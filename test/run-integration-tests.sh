8e5af59ec2f436f7f8172028b720eda4abcb7c02
Arnold Bechtoldt <arnold.bechtoldt@inovex.de>
2017-08-11 11:20:14 +0200

    use exaple config for tests

--- test/run-integration-tests.sh
+++ test/run-integration-tests.sh
@@ -15,7 +15,7 @@ cleanup() {
 trap cleanup INT TERM
 
 echo "=> Starting exporter"
-./mqtt_blackbox_exporter -config.file test/test.yaml &
+./mqtt_blackbox_exporter -config.file config.yaml.dist &
 service_pid=$!
 
 echo "=> Waiting 5s"
